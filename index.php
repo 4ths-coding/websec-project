5b9bb47ad6426319c02ba89ace876ee6de33075f
4ths-coding <spareemail4th@gmail.com>
2025-04-04 16:29:03 +0800

    Change

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Secure Login System</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form method="POST" action="login.php">
+        Username: <input type="text" name="username" required><br>
+        Password: <input type="password" name="password" required><br>
+        <input type="submit" value="Login">
+    </form>
+
+    <p>Don't have an account? <a href="register.html">Register here</a></p>
+</body>
+</html>
